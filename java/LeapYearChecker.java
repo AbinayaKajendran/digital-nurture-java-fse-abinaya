73b0dc6231aecd463f49c602fc5db85e0a92c2b5
AbinayaKajendran <abikajendran09012006@gmail.com>
2026-06-03 21:10:12 +0530

    Update LeapYearChecker.java

--- java/LeapYearChecker.java
+++ java/LeapYearChecker.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Program04_LeapYearChecker {
+public class LeapYearChecker {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
