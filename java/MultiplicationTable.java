457b17cf297ec616229d30ab1de61b9b8e42b13f
AbinayaKajendran <abikajendran09012006@gmail.com>
2026-06-03 21:10:58 +0530

    Update MultiplicationTable.java

--- java/MultiplicationTable.java
+++ java/MultiplicationTable.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Program05_MultiplicationTable {
+public class MultiplicationTable {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
