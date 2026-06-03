ef848db397dd04aca37cb921ee67a41757b110ae
AbinayaKajendran <abikajendran09012006@gmail.com>
2026-06-03 21:06:20 +0530

    Create DataTypeDemo.java

---
+++ java/DataTypeDemo.java
@@ -0,0 +1,16 @@
+public class Program06_DataTypeDemo {
+    public static void main(String[] args) {
+
+        int age = 20;
+        float percentage = 89.5f;
+        double salary = 35000.75;
+        char grade = 'A';
+        boolean passed = true;
+
+        System.out.println("Integer Value : " + age);
+        System.out.println("Float Value   : " + percentage);
+        System.out.println("Double Value  : " + salary);
+        System.out.println("Char Value    : " + grade);
+        System.out.println("Boolean Value : " + passed);
+    }
+}
